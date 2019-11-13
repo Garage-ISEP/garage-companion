6b7ea53e4639c27e4dc1d355cf301110848c59af
Jules Ponsaillé <jules.ponsaille@isep.fr>
2019-11-13 22:26:37 +0100

    getting pages

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,16 +1,37 @@
-import { h } from 'preact';
+import { h, Component } from 'preact';
 import { Link } from 'preact-router/match';
+import { getCalendarList } from '../../utils/garage-api';
 import style from './style';
 
-const Header = () => (
-	<header class={style.header}>
-		<h1>Preact App</h1>
-		<nav>
-			<Link activeClassName={style.active} href="/">Home</Link>
-			<Link activeClassName={style.active} href="/profile">Me</Link>
-			<Link activeClassName={style.active} href="/profile/john">John</Link>
-		</nav>
-	</header>
-);
+class Header extends Component {
+	state = {
+		calendars: []
+	}
+
+	componentDidMount() {
+		getCalendarList().then((calendars) => {
+			this.setState({
+				calendars
+			});
+		});
+	}
+
+	render({}, { calendars }) {
+		return (
+			<header class={style.header}>
+				<h1>Preact App</h1>
+				<nav>
+					<Link activeClassName={style.active} href="/">Home</Link>
+					{
+						calendars.map((calendar, index) => (
+							<Link key={index} activeClassName={style.active} href={`/calendar/${calendar.id}`}>{ calendar.summary }</Link>
+						))
+					}
+				</nav>
+			</header>
+		)
+	}
+}
+
 
 export default Header;
