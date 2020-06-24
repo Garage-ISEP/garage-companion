676724109560f160d1e1852b8a28339621a9241b
Charles-Emmanuel <charlesemmanuel.bray@gmail.com>
2020-06-25 00:09:40 +0200

    better tutorial

--- src/components/tutorial/index.js
+++ src/components/tutorial/index.js
@@ -9,33 +9,31 @@ import DialogTitle from '@material-ui/core/DialogTitle';
 import { useState } from 'preact/hooks';
 import { makeStyles } from '@material-ui/core/styles';
 import LinearProgress from '@material-ui/core/LinearProgress';
+import Box from '@material-ui/core/Box';
 
 const versionTuto = 1;
 const message = [
     {
-        image: '../../assets/tutorial/calendarNavTutorial.png',
+        image: "../../assets/tutorial/logoGarage.png",
+        title : "Bienvenue",
+        content : "Bienvenue sur Garage Companion. Ce site à pour objectf de permettre aux isépiens de se renseigner et de pouvoir s'inscrire facilement aux évènements de GarageISEP. Souhaitez vous voir en détail les différentes fonctionnalités offertes par ce site ?"
+    },
+    {
+        image: '../../assets/tutorial/calendarNav.png',
         title : "Barre de navigation",
-        content : "La barre de navigation va permettre de filtrer les évènements en fonction du lab qui les organise. Il suffit simplement de cliquer sur les labs les sélectionner/désélectionner. \n"
+        content : "La barre de navigation va permettre de filtrer les évènements en fonction du lab qui les organise. Il suffit simplement de cliquer sur les labs les sélectionner/désélectionner. \r\n"
         + "Le bouton 'Préviously' permet quand à lui de choisir si l'on affiche ou non les anciens évènements " 
 
     },
     {
-        image: "",
-        title : "titre 1",
-        content : "contenu 1"
-
+        image: "../../assets/tutorial/event.png",
+        title : "Les évènements",
+        content : "Pour chacun des évènements, il est possible de se renseigner en appuyant sur le bouton \"EN SAVOIR PLUS\" ou s'inscrire en appuyant sur le bouton \"S'INSCRIRE\""
     },
     {
-        image: "",
-        title : "titre 2",
-        content : "contenu 2"
-
-    },
-    {
-        image: "",
-        title : "titre 3",
-        content : "contenu 3"
-
+        image: "../../assets/tutorial/addParticipantDialog.png",
+        title : "S'inscrire",
+        content : "Une fois que vous avez appuyé sur le bouton \"S'INSCRIRE\", une fenêtre s'ouvre et il ne vous reste plus qu'à renseigner votre adresse mail."
     },
 ];
 
@@ -70,17 +68,17 @@ export default function Tutorial(){
         }
     };
 
-    const isDone = () => { //verify if tuto is already done
-        if(localStorage.getItem("versionTutorialDone")){
-            if(localStorage.getItem("versionTutorialDone") == versionTuto){
-                handleClose();
-            }else{
-                localStorage.versionTutorialDone = versionTuto;
-            }
-        }else{
-            localStorage.versionTutorialDone = versionTuto;
-        }
-    }
+    // const isDone = () => { //verify if tuto is already done
+    //     if(localStorage.getItem("versionTutorialDone")){
+    //         if(localStorage.getItem("versionTutorialDone") == versionTuto){
+    //             handleClose();
+    //         }else{
+    //             localStorage.versionTutorialDone = versionTuto;
+    //         }
+    //     }else{
+    //         localStorage.versionTutorialDone = versionTuto;
+    //     }
+    // }
 
     return (
         <div>
@@ -92,7 +90,7 @@ export default function Tutorial(){
                 disableBackdropClick={true}
                 fullwidth={true}
                 maxWidth = {false}
-                onEnter = {isDone}
+                // onEnter = {isDone}
                 className = {style.dialog}
                 BackdropProps={{
                     classes :{
@@ -109,7 +107,7 @@ export default function Tutorial(){
 
                 <DialogTitle id="alert-dialog-title">{message[idMessage].title}</DialogTitle>
 
-                <img className={style.imageTuto} src = {message[idMessage].image}/>
+                <Box className={style.imageBox}><img className={style.imageTuto} src = {message[idMessage].image}/></Box>
 
                 <DialogContent>
                     <DialogContentText id="alert-dialog-description">
